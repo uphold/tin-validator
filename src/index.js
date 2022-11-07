f06ad1b7d3a5bd2bd5e287df9baf3ae9e68fe080
Kevin Alvarez <kevin.alvarez@uphold.com>
2022-11-07 19:04:38 -0400

    Improve TIN validator

--- src/index.js
+++ src/index.js
@@ -14,10 +14,33 @@ import { isValid as isValidItin, mask as maskItin } from 'itin-validator';
 import { isValid as isValidSsn, mask as maskSsn } from 'ssn-validator';
 
 /**
+ * Excludes repeated numbers as TIN e.g. 111111111.
+ */
+
+const repeatedNumbers = Array.from({ length: 10 }, (_, i) => i).map(current => String(current).repeat(9));
+
+/**
+ * Excludes ascending and descending sequence as TIN e.g. 123456789.
+ */
+
+const sequence = Array.from({ length: 10 }, (_, i) => i).reduce((acc, current) => acc + current, '').repeat(2);
+const reverseSequence = sequence.split('').reverse().join('');
+
+/**
  * Export `isValid` function.
  */
 
 export function isValid(value) {
+  const sanitizedValue = value.replace(/\D/g, '');
+
+  if (repeatedNumbers.indexOf(sanitizedValue) !== -1) {
+    return false;
+  }
+
+  if (sequence.includes(sanitizedValue) || reverseSequence.includes(sanitizedValue)) {
+    return false;
+  }
+
   return isValidSsn(value) || isValidItin(value) || isValidEin(value);
 }
 
